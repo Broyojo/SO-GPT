d7ba2792102619f06947275b3773643cb04cee93
Broyojo <dha@xoba.com>
2023-03-07 00:07:59 -0500

    tweaked training routine and added wandb

--- train.py
+++ train.py
@@ -17,7 +17,9 @@ tokenizer.pad_token = tokenizer.eos_token
 model = AutoModelForCausalLM.from_pretrained(
     MODEL_NAME, pad_token_id=tokenizer.pad_token_id
 )
-data_collator = DataCollatorForLanguageModeling(tokenizer, mlm=False)
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer, mlm=False
+)  # fix some performance thing here
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 
@@ -26,10 +28,7 @@ def process(example):
     question_body = example["question_body"]
     answer_body = example["answer_body"]
 
-    concatenated = (
-        f"Title:{title}\nQuestion:{question_body}\nAnswer:{answer_body}<|endoftext|>"
-    )
-
+    concatenated = f"Title: {title}\nQuestion:\n{question_body}\nAnswer:\n{answer_body}<|endoftext|>"
     return {"text": concatenated}
 
 
@@ -38,8 +37,8 @@ def encode(example):
         example["text"],
         truncation=True,
         max_length=CONTEXT_LENGTH,
-        return_overflowing_tokens=True,
-        return_length=True,
+        # return_overflowing_tokens=True,
+        # return_length=True,
     )
 
     return {"input_ids": encoded["input_ids"]}
@@ -76,14 +75,13 @@ dataset = (
 
 print(dataset)
 
-# tokens = dataset["train"][8]["input_ids"]
+tokens = dataset["train"][6]["input_ids"]
 
-# print(tokenizer.decode(tokens))
-# print("len", len(tokens))
+print(tokenizer.decode(tokens))
+print("len", len(tokens))
 
-# quit()
 training_args = TrainingArguments(
-    output_dir="models/gpt2-python",
+    output_dir=f"models/{MODEL_NAME}-python-truncate-natural-prompt",
     half_precision_backend="auto",
     fp16=True,
     fp16_full_eval=True,
@@ -93,6 +91,12 @@ training_args = TrainingArguments(
     evaluation_strategy="steps",
     full_determinism=True,
     seed=SEED,
+    logging_steps=50,
+    save_steps=5000,
+    eval_steps=5000,
+    report_to="wandb",
+    save_total_limit=5,
+    run_name=f"{MODEL_NAME}-python-truncate-natural-prompt",
 )
 
 trainer = Trainer(
